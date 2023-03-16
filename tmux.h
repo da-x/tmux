3c7ca666f937c0990c21668829174e0e730460ad
Dan Aloni <alonid@gmail.com>
2023-03-16 22:47:11 +0200

    Add 'pane_unseen_changes' format
    This commit introduces a new pane_unseen_changes format. When a pane has
    seen input while it was in some mode, it sets the flags to
    PANE_UNSEEN_CHANGES. It is cleared when exiting the mode.

--- tmux.h
+++ tmux.h
@@ -961,6 +961,7 @@ struct window_pane {
 #define PANE_EMPTY 0x800
 #define PANE_STYLECHANGED 0x1000
 #define PANE_RESIZENOW 0x2000
+#define PANE_UNSEEN_CHANGES 0x4000
 
 	int		 argc;
 	char	       **argv;
